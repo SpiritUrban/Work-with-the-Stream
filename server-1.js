cf9272d1da6568f4f3aff68c34c1a43d42f44387
Vitaliy-Dyachuk <shadespiritenator@gmail.com>
2016-07-30 01:28:22 +0300

    Module

--- server-1.js
+++ server-1.js
@@ -1,117 +1,17 @@
-var fs = require('fs');
 
-/* 
-
-fs.writeFileSync('test.txt', 'Привет'); // Запис-Перезапис 
-
-// Асинхронний Запис-Перезпис
-fs.writeFile("test.txt", "Привет!", function(err) {
-    if(err) {
-        console.log(err);
-    } else {
-        console.log("Файл сохранен.");
-    }
-}); 
+var fs    = require('fs');
+var lib = require('./my_modules/lib.js');
 
 
 
-fs.appendFileSync('test.txt', ' Привет'); // Запис-ДОЗАПИС 
+// Всі методи "lib"
+// lib.createMatrix(80000); // Створити тестовий файл (з числами від 0 до N )
+// lib.streamRead();        // Потокове читання тестового файлу і вивід інформації 
+// lib.streamWrite();       // Потооковий запис файлу
 
-// Асинхронний Запис-ДОЗАПИС 
-fs.appendFile("test.txt", " Привет!!!000/", function(err) {
-    if(err) {
-        console.log(err);
-    } else {
-        console.log("Файл сохранен.");
-    }
-});
-
-// Читання
-var text = fs.readFileSync('test.txt', 'utf8');
-//console.log(text);
-
-fs.readFile('test.txt', {encoding: 'utf8'}, function (err, data) {
-    if (err) throw err;
-    console.log(data);
-});
-
-*/
-
-function createMatrix(quantity) {
-	fs.writeFileSync('test.txt', ''); // Запис-Перезапис 
-
-	for (var i=0; i < quantity; i++) {
-		console.log(i);
-		fs.appendFileSync('test.txt', ' ' + i); // Запис-ДОЗАПИС 
-	}
-
-	var text = fs.readFileSync('test.txt', 'utf8');
-	console.log(text);
-};
-
-//createMatrix(80000);
+ lib.streamReadWrite();    // Потокові паралельні читання + запис
 
 
-function streamRead() {
-	console.log('\n \n \n ------------------------------------- START -------------------------------------');
 
-	var stream = fs.createReadStream('test.txt', {encoding: 'utf8'});
-	var counter = 0;
-
-	stream.on('readable', function() {
-	    var buf;
-	    while ((buf = stream.read()) !== null) {
-	    	//console.log(' \n');
-	        console.log(' Прочитано - ' + buf.length  + ' символів');
-	        counter++;        
-	    }
-	});
-
-	stream.once('end', function() {
-	    console.log('\n Читання виконано успішно.');
-	    console.log('\n Виконано за ' + counter + ' ітерацій ');
-	    console.log('\n -------------------------------------  END  ------------------------------------- \n \n');
-	});
-	// Читає по 64 кілобайта
-};
-
-//streamRead();
-
-
-function streamWrite() {
-
-	var writer = fs.createWriteStream('test-2.txt', {flags: 'w'});
-
-	writer.on('finish', function() {
-	    console.error('Запись выполнена успешно.');
-	});
-
-	var i = 0;
-
-	function write() {
-	    do {
-
-	        var ok = writer.write('Тест, #' + i + '!\n');
-
-	        if (!ok) {
-	            writer.once('drain', write);
-	            break;
-	        }
-
-	        i++;
-
-	    } while (i < 10 && ok);
-
-	    if (i === 10) {
-	        writer.end('===== Конец =====\n');
-	    }
-	}
-	write();
-};
-
-// streamWrite();
-
-function streamReadWrite() {
-	//console.log(8);
-};
-streamReadWrite();
+lib.First(' Hello! ');  // Тестовий
+lib.Second('Hello!2');  // Тестовий
